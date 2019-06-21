18a9736cf479dbf735315e779114b5f6380d06a1
Аружан Туткенова <a.tutkenova@air-aruzan.dar.lcl>
2019-06-21 14:36:10 +0600

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { TodoListComponent } from './todo-list/todo-list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
-  
   ],
   imports: [
-    FormsModule,
     BrowserModule
   ],
   providers: [],
